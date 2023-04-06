e0db7d6b7e7143e9c68b8852b9b85d44140846bb
Wullems Bart <wullemsb@gmail.com>
2023-04-06 12:47:06 +0200

    Remove startup.cs

--- code/session-8/GraphQL/Program.cs
+++ code/session-8/GraphQL/Program.cs
@@ -1,26 +1,57 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using ConferencePlanner.GraphQL.Attendees;
+using ConferencePlanner.GraphQL.Data;
+using ConferencePlanner.GraphQL.DataLoader;
+using ConferencePlanner.GraphQL.Sessions;
+using ConferencePlanner.GraphQL.Speakers;
+using ConferencePlanner.GraphQL.Tracks;
+using ConferencePlanner.GraphQL.Types;
+using Microsoft.EntityFrameworkCore;
 
-namespace ConferencePlanner.GraphQL
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddPooledDbContextFactory<ApplicationDbContext>(
+                options => options.UseSqlite("Data Source=conferences.db"));
+
+builder.Services.AddGraphQLServer()
+    //https://chillicream.com/docs/hotchocolate/v13/integrations/entity-framework/#working-with-a-pooled-dbcontext
+    .RegisterDbContext<ApplicationDbContext>(DbContextKind.Pooled)
+    .AddQueryType(d => d.Name("Query"))
+        .AddTypeExtension<AttendeeQueries>()
+        .AddTypeExtension<SpeakerQueries>()
+        .AddTypeExtension<SessionQueries>()
+        .AddTypeExtension<TrackQueries>()
+    .AddMutationType(d => d.Name("Mutation"))
+        .AddTypeExtension<AttendeeMutations>()
+        .AddTypeExtension<SessionMutations>()
+        .AddTypeExtension<SpeakerMutations>()
+        .AddTypeExtension<TrackMutations>()
+    .AddSubscriptionType(d => d.Name("Subscription"))
+        .AddTypeExtension<AttendeeSubscriptions>()
+        .AddTypeExtension<SessionSubscriptions>()
+    .AddType<AttendeeType>()
+    .AddType<SessionType>()
+    .AddType<SpeakerType>()
+    .AddType<TrackType>()
+    .AddGlobalObjectIdentification()
+    .AddQueryFieldToMutationPayloads()
+    .AddFiltering()
+    .AddSorting()
+    .AddInMemorySubscriptions()
+    .AddDataLoader<SpeakerByIdDataLoader>()
+    .AddDataLoader<SessionByIdDataLoader>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+    app.UseDeveloperExceptionPage();
 }
+
+app.UseWebSockets();
+app.UseRouting();
+
+app.MapGraphQL();
+
+app.Run();
